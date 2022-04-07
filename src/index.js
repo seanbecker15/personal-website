cd9550045e0889b1ae3d630e55103b2a96994b9a
Sean Becker <sean.becker15@gmail.com>
2022-04-06 23:38:57 -0500

    Squashed commit of the following:
    commit 41d7c2b4034292b84169966bf9f28f1e46e6ee07
    Author: Sean Becker <sean.becker15@gmail.com>
    Date:   Wed Apr 6 23:36:43 2022 -0500
    
        introduce react (slowly)
    
    commit dd23d96ad3642ad2e93527bea559f058cc7a56ab
    Author: Sean Becker <sean.becker15@gmail.com>
    Date:   Wed Apr 6 23:36:23 2022 -0500
    
        format files using prettier
    
    commit bb25b21a790927dd34bac8e4fa68fd2393340618
    Author: Sean Becker <sean.becker15@gmail.com>
    Date:   Wed Apr 6 23:35:54 2022 -0500
    
        initialize nodejs and js environment

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const createRootById = (id) => {
+  const element = document.getElementById(id);
+  const root = ReactDOM.createRoot(element);
+  return root;
+};
+
+const inactiveProjectsReactRoot = createRootById("inactive-projects-container");
+
+const projects = [
+  {
+    url: "https://webapps1.chicago.gov/chicagohouseshare/",
+    name: "House Share Registration Portal - BACP Chicago"
+  },
+  {
+    url: "./projects/caseincident.html",
+    name: "CASE Records Management System - Chicago Police Department"
+  },
+  {
+    url: "./projects/automatedcallform.html",
+    name: "ACF Election Software - Cook County"
+  },
+  {
+    url: "./projects/buzz.html",
+    name: "Drink Please (formerly bzzer.io)"
+  },
+  {
+    url: "https://sumospinnerv2.herokuapp.com/",
+    name: "Sumo Spinner"
+  },
+  {
+    url: "./projects/lateplateme.html",
+    name: "Lateplateme - Tool for requesting food on the go"
+  },
+  {
+    url: "https://github.com/seanbecker15/we-eat",
+    name: "We Eat - Yelp alternative built during Purdue CS 307"
+  },
+  {
+    url: "https://github.com/seanbecker15/cspan-scraper",
+    name: "Purdue University CSPAN Research"
+  },
+];
+
+inactiveProjectsReactRoot.render(
+  React.createElement("ul", {
+    children: projects.map((p) =>
+      React.createElement("li", {
+        children: React.createElement("a", { href: p.url, children: p.name }),
+      })
+    ),
+  })
+);
