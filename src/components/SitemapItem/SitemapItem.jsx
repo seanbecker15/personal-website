2de9a0aefd996c8965fb1c780328f323e26781a4
Sean Becker <sean.becker15@gmail.com>
2023-01-26 22:15:46 -0600

    format sitemap items

--- src/components/SitemapItem/SitemapItem.jsx
+++ src/components/SitemapItem/SitemapItem.jsx
@@ -1,7 +1,51 @@
-export default function SitemapItem(props) {
+// @flow
+
+import type { Node } from "react";
+
+import "./styles.scss";
+
+import { createElement } from "react";
+
+type Props = {
+  level: number,
+  title: string,
+  children: Node,
+};
+
+export default function SitemapItem(props: Props): Node {
+  const { level } = props;
+
   return (
     <div className="SitemapItem" title={props.title}>
-      {"--".repeat(props.level)} {props.children}
+      {renderPrefix(level)}
+      {props.children}
     </div>
   );
+}
+
+function renderPrefix(level: number) {
+  if (level === 0) {
+    return " ";
+  }
+
+  if (level === 1) {
+    return "|-- ";
+  }
+
+  const arr = new Array((level - 2) * 4 + 3).fill(null);
+  return (
+    <>
+      {"|"}
+      {arr.map(() => (
+        <>
+          <Space />
+        </>
+      ))}
+      {"|-- "}
+    </>
+  );
+}
+
+function Space() {
+  return <>&nbsp;</>;
 }
