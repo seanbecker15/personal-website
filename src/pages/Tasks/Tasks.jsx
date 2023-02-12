7dae4f5e246169308912aace8b95ba5d46a61d08
Sean Becker <sean.becker15@gmail.com>
2023-02-12 14:00:59 -0600

    update tasks

--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -1,11 +1,20 @@
 import { Fragment, useEffect, useRef } from "react";
 import * as Eisenhower from "../../components/EisenhowerMatrix";
 import SitemapItem from "../../components/SitemapItem";
+import List from "@ui/List";
+import ListItem from "@ui/ListItem";
 import "./styles.scss";
 
 export default function Tasks() {
   return (
     <div className="Tasks">
+      <h2>Tasks</h2>
+      <List>
+        <ListItem>Move to component library</ListItem>
+        <ListItem>Add FuboTV SmartTV work</ListItem>
+        <ListItem>Update things I am learning about</ListItem>
+        <ListItem>Write a blog</ListItem>
+      </List>
       <Eisenhower.Matrix size={300} xOffset={100} yOffset={100}>
         <Eisenhower.Dot urgency={0} importance={0} title="Example" />
       </Eisenhower.Matrix>
