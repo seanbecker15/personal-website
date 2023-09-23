4ec54b473a2299ae39bc03b413eac8fa3c1c996e
Sean Becker <sean.becker15@gmail.com>
2023-09-23 17:01:29 -0500

    update content

--- src/routes/about-me/index.js
+++ src/routes/about-me/index.js
@@ -1,8 +1,30 @@
+import { List, ListItem } from "@ui/atoms";
+
 export default function AboutMe() {
-  return <div className="AboutMe">
-    <h2>About Me</h2>
-    This page is coming soon.
-  </div>;
+  return (
+    <div className="AboutMe">
+      <h2>About Me</h2>
+      {"Here's a little bit of information about me:"}
+      <List>
+        <ListItem>I grew up in Vernon Hills, IL</ListItem>
+        <ListItem>
+          I went to Purdue University where I majored in Computer Science and
+          minored in Business Management
+        </ListItem>
+        <ListItem>
+          I try to take at least one ski trip every year, usually on the west
+          coast of the US
+        </ListItem>
+        <ListItem>
+          I frequently listen podcasts including Lex Fridman, Andrew Huberman,
+          Joe Rogan, and Sam Harris
+        </ListItem>
+        <ListItem>
+          I enjoy building all things related to technology! Please reach out if you have an idea and want advice on how to build it {':)'}
+        </ListItem>
+      </List>
+    </div>
+  );
 }
 
 export const aboutMeLoader = () => null;
