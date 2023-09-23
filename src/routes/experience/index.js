5bbdd9497cf2f894d8526a7d8f4a9da4461c7085
Sean Becker <sean.becker15@gmail.com>
2023-09-23 16:10:25 -0500

    integrate prior changes

--- src/routes/experience/index.js
+++ src/routes/experience/index.js
@@ -5,28 +5,26 @@ export default function Experience() {
     <div className="Experience">
       <h2>Current employment</h2>
       <h3>FuboTV</h3>
+
       <List>
         <ListItem>
-          Pushed along process improvements, standards improvements, and
-          modernization of code practices.
+          Process & standards improvements, modernization of code practices
+        </ListItem>
+        <ListItem>Performed overhaul of remote-control focus system.</ListItem>
+        <ListItem>
+          Contributed to several features during application rewrite, including
+          the navigation sidebar and playback controls
         </ListItem>
         <ListItem>
-          Performed large overhaul of remote control focus system.
-        </ListItem>
-        <ListItem>
-          Contributed heavily to several features of application rewrite,
-          including the navigation sidebar and playback controls.
-        </ListItem>
-        <ListItem>
-          Sped up development efficiency in key areas including VOD Playback &
-          Client Side Ad Insertion (CSAI)
+          Sped up development efficiency in key areas including VOD Playback,
+          Client Side Ad Insertion (CSAI), Xbox, and LG (WebOS)
         </ListItem>
       </List>
-      <h3>{'Fubo Gaming (former subsidiary of FuboTV)'}</h3>
+      <h3>{"Fubo Gaming (former subsidiary of FuboTV)"}</h3>
       <List>
         <ListItem>
           Assisted in shaping processes to scale team from 5 engineers to 15
-          engineers.
+          engineers
         </ListItem>
         <ListItem>
           Created development pipeline and wrote template for making and
@@ -34,22 +32,23 @@ export default function Experience() {
         </ListItem>
         <ListItem>
           Performed auth rewrite & integration between sportsbook and TV
-          product. Coordinated effort between white-label provider, in-house
-          designers, in-house frontend team, and in-house platform team.
+          product; Coordinated effort between white-label provider, in-house
+          designers, in-house frontend team, and in-house platform team
         </ListItem>
         <ListItem>
           Helped pass regulatory review in order to launch sportsbook in three
           states (Iowa, Arizona, and New Jersey)
         </ListItem>
         <ListItem>
-          Led Trustly payment integration and licensing from technical side.
+          Led Trustly payment integration and licensing from technical
+          perspective
         </ListItem>
       </List>
-      
+
       <h2>Prior employment</h2>
       <List>
         <ListItem>
-          For information on prior employment, you can{' '}
+          For information on prior employment, you can{" "}
           <Anchor to="./assets/docs/resume.pdf">view my resume here</Anchor>.
         </ListItem>
       </List>
