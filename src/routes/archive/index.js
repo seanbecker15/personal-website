dbe9703f2d4cf00b2ae968ce6def4ebdea000fa1
Sean Becker <sean.becker15@gmail.com>
2023-09-23 16:20:56 -0500

    add header to archive

--- src/routes/archive/index.js
+++ src/routes/archive/index.js
@@ -2,6 +2,7 @@ import { ProjectList } from '@ui/molecules';
 
 export default function Archive() {
   return <div className="Archive">
+    <h2>Archive</h2>
     <ProjectList filter={({ active }) => !active} />
   </div>;
 }
