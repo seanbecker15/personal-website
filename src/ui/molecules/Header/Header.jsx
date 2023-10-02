db6a64d59e7a2ba375f5a9683a04bd313b2dd610
Sean Becker <sean.becker15@gmail.com>
2023-10-02 09:55:26 -0500

    improve image loading

--- src/ui/molecules/Header/Header.jsx
+++ src/ui/molecules/Header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="Header">
       <h1>Sean Becker - Technology</h1>
-      <img className="Header__img" src={"/assets/img/profile.jpg"} />
+      <img className="Header__img" src={"/assets/img/profile.jpeg"} />
     </div>
   );
 }
