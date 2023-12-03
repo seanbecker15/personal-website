aff5951afb8e5eabd31da12e727d245884a775fe
Sean Becker <sean.becker15@gmail.com>
2023-12-03 02:16:05 -0600

    fix build

--- components/legacy/components/Anchor.js
+++ components/legacy/components/Anchor.js
@@ -1,12 +1,3 @@
 export default function Anchor(props) {
-  return (
-    <a
-      href={props.to}
-      rel={props.isExternal ? 'noreferrer' : undefined}
-      target={props.isExternal ? "_blank" : undefined}
-      referrerPolicy={props.isExternal ? "no-referrer" : undefined}
-    >
-      {props.children}
-    </a>
-  );
+  return <a href={props.to}>{props.children}</a>;
 }
