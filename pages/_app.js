f71f11457b79988906dcea493fe70113247e85b3
Sean Becker <sean.becker15@gmail.com>
2025-12-16 00:10:32 -0600

    Add plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,17 @@ function MyApp({ Component, pageProps }) {
           gtag('config', 'G-RBGFCZ9RGS');
         `}
       </Script>
+      <Script
+        async
+        src="https://plausible.io/js/pa-25S1Gw-mrJAC0dncwddaL.js"
+        strategy="afterInteractive"
+      />
+      <Script id="plausible-init" strategy="afterInteractive">
+        {`
+          window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+          plausible.init()
+        `}
+      </Script>
     </>
   );
 }
